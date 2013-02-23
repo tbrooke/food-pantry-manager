de681d23071d72ae3cef342e2a70f0cccb1931f4
tbrooke <tom.brooke@gmail.com>
2013-02-22 23:55:10 -0500

    added sudo to rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ require "cap_bootstrap/capistrano"
 
 server "192.81.219.43", :web, :app, :db, primary: true
 
-set :user, "deployer"
+set :user, "deploy"
 set :application, "food-pantry-manager"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 set :scm, "git"
 set :repository, "git@github.com:tbrooke/food-pantry-manager.git"
-set :branch, "master"
+set :branch, "ui-improvements"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
