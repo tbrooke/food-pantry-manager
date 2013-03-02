709e06ed2b9e323dc8a2f2ca201032ca8e38620e
tbrooke <tom.brooke@gmail.com>
2013-03-01 21:36:27 -0500

    fixed merge conflict

--- app/controllers/neighbors_controller.rb
+++ app/controllers/neighbors_controller.rb
@@ -86,20 +86,12 @@ class NeighborsController < ApplicationController
 
   def allowable
     [
-<<<<<<< HEAD
       :city, :close_date, :date_of_proof, :first_name, :middle_name, :street, :apt,
       :food_stamps, :last_name, :monthly_income, :notes, :house_rank,
       :open_date, :phone, :proof_of_residency_type, :email, :sex, :rent, :residency_date,
       :proof_of_residency_type, :smokes, :spouse, :ssn, :state, :street,
       :utilities, :zip, :age, :created_at, :updated_at, :neighbors_attributes
-=======
-        :city, :close_date, :date_of_proof, :first_name, :middle_name,  :street,
-        :food_stamps, :last_name, :monthly_income, :notes, :number_of_children,
-        :open_date, :phone, :proof_of_residency_type, :rent, :residency_date,
-        :proof_of_residency_type, :smokes, :spouse, :ssn, :state, :street,
-        :utilities, :zip ,:created_at, :updated_at , :neighbors_attributes
->>>>>>> ui-improvements
-    ]
+   ]
 
 
 
