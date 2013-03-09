b325fa92e9ae5d4130c8ddffce16c668aee95507
tbrooke <tom.brooke@gmail.com>
2013-03-09 14:52:54 -0500

    setup migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305154330) do
+ActiveRecord::Schema.define(:version => 20130309193928) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(:version => 20130305154330) do
   add_index "donations", ["donor_id"], :name => "index_donations_on_donor_id"
 
   create_table "donors", :force => true do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
