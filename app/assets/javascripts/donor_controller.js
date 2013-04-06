169742ed1e3e8ff57a3a4c2e9553fab0251132c2
tbrooke <tom.brooke@gmail.com>
2013-04-06 07:41:12 -0400

    added full angular stack

--- app/assets/javascripts/donor_controller.js
+++ app/assets/javascripts/donor_controller.js
@@ -1,19 +1,23 @@
 
-//var app = angular.module("Donor", ["ngResource"]);
+var app = angular.module("Donor", ["ngResource"]);
 
-//app.factory("Donor", [
-//    "$resource", function($resource) {
-//        return $resource("/donors/:id", {}, {
-//            query: {
-//                method: "GET", isArray:true
-//            }
-//        });
+app.factory("Donor", [
+    "$resource", function($resource) {
+        return $resource("/donors/:id", {}, {
+            query: {
+                method: "GET", isArray:true
+            }
+        });
 
 //angular.module('Donor', ['ngResource']);
 
 angular.module('donor.services', ['rails']);
 angular.module('donor.services').factory('Donor', ['railsResourceFactory', function (railsResourceFactory) {
     return railsResourceFactory({url: '/donors', last_name: 'donor'});
+}]);
+
+angular.module('donor.controllers').controller('DonorCtrl', [$scope, 'Book', function ($scope, Donor){
+    var donors = Donor.query({last_name: 'Bullard'})
 }]);
 
 //app.factory("Donor", function($resource) {
